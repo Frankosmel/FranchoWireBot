8d47ac9ed9dbc568c628c5631d916ac6569ea52b
Frankosmel <130179322+Frankosmel@users.noreply.github.com>
2025-08-06 14:02:16 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Token del bot
+BOT_TOKEN = '8334285648:AAFui_V95j-RgnTtkYQO_-SdgYCUjX6oBo4'
+
+# ID del administrador autorizado
+ADMIN_ID = 1383931339  # Cámbialo si deseas otro ID
+
+# Ruta del script bash para crear clientes
+SCRIPT_PATH = '/home/ubuntu/francho_wire/crear_cliente.sh'  # Asegúrate de que esta ruta sea correcta
+
+# Carpeta donde se guardan los archivos generados por el script
+CLIENTS_DIR = '/home/ubuntu/francho_wire/clientes'  # Carpeta donde se almacenan .conf y .png
